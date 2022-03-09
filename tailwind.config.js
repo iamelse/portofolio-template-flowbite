6f318cc656afc928b9d8015cb10a9413fb30ce27
Lana Septiana <lanaseptiana2@gmail.com>
2022-03-09 08:52:42 +0700

    Starter Project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+    "./public/**/*.{html, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
